4ea1a44e2069a0493721e3108fd620f4f16eea60
Samuel Obour Mensah <msamuelobourmensah@gmail.com>
2024-04-04 22:25:35 +0000

    first commit

---
+++ android/app/src/main/kotlin/com/example/pill_set/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.pill_set
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ lib/view/widgets/colors.dart
@@ -0,0 +1,6 @@
+import 'package:flutter/material.dart';
+
+Color greenColor = const Color(0xFF34C756);
+Color greyColor = const Color(0xff555555);
+Color ashColor = const Color(0xFFBCBCBC);
+Color whiteColor = const Color(0xffffffff);
